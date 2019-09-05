d15be939fe30a2c4a51dfdfbcc28b5226cb55e9c
SumitLagad <sumitlagad7@gmail.com>
2019-09-05 14:43:53 +0530

    React Router Testing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import { BrowserRouter,Route,Link } from 'react-router-dom';
+
+const RouteOne = () => {
+    return(
+        <div>
+            Route One
+
+            <Link to="/routetwo">page two</Link>
+        </div>
+    )
+};
+
+
+const RouteTwo = () => {
+    return(
+        <div>
+            Route Two
+            <Link to="/">Page One</Link>
+        </div>
+    )
+};
+
+
 
 const App = () => {
     return (
         <div>
-            App Component
+            <BrowserRouter>
+                <Route path="/" exact component={RouteOne} />                
+     
+                <Route path="/routetwo"  component={RouteTwo} />
+            </BrowserRouter>
         </div>
     );
 
