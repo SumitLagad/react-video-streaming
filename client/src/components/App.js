2d2e218c54c9194520661a253058325ea7050bf6
SumitLagad <sumitlagad7@gmail.com>
2019-09-06 10:31:31 +0530

    Component scaffolding with header added

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,35 +1,26 @@
 import React from 'react';
-import { BrowserRouter,Route,Link } from 'react-router-dom';
-
-const RouteOne = () => {
-    return(
-        <div>
-            Route One
-
-            <Link to="/routetwo">page two</Link>
-        </div>
-    )
-};
-
-
-const RouteTwo = () => {
-    return(
-        <div>
-            Route Two
-            <Link to="/">Page One</Link>
-        </div>
-    )
-};
+import { BrowserRouter,Route } from 'react-router-dom';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamCreate from './streams/StreamCreate';
+import Header from './Header';
 
 
 
 const App = () => {
     return (
-        <div>
+        <div className="ui container">
             <BrowserRouter>
-                <Route path="/" exact component={RouteOne} />                
-     
-                <Route path="/routetwo"  component={RouteTwo} />
+                <div>
+                    <Header />
+                    <Route path="/" exact component={StreamList} />
+                    <Route path="/streams/new" component={StreamCreate} />
+                    <Route path="/streams/edit" component={StreamEdit} />
+                    <Route path="/streams/delete" component={StreamDelete} />
+                    <Route path="/streams/show" component={StreamShow} />
+                </div>
             </BrowserRouter>
         </div>
     );
