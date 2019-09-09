477c3fd1a9f28a3250515f556fd357b672bb9423
SumitLagad <sumitlagad7@gmail.com>
2019-09-09 10:26:39 +0530

    Added redux store inspect tool to app.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import reducers from './reducers';
 
 import App from './components/App';
 
-const store = createStore(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducers, composeEnhancers(applyMiddleware()));
 
 ReactDOM.render(
     <Provider store={store}>
